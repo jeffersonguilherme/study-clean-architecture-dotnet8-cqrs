817f9a824f181a3f63701152d621321a216b41ca
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-08-03 21:39:12 -0300

    feat(identity): adiciona contrato para validação de credenciais
    - adiciona o método ValidateCredentialsAsync à interface IIdentityService
    - define o contrato para validação de credenciais de autenticação
    - retorna o status da autenticação, identificador, e-mail e roles do usuário
    - estabelece a abstração necessária para o fluxo de login e geração de tokens JWT

--- src/MyProductApp.Application/Interfaces/Identity/IIdentityService.cs
+++ src/MyProductApp.Application/Interfaces/Identity/IIdentityService.cs
@@ -3,4 +3,5 @@ namespace MyProductApp.Application.Interfaces.Identity;
 public interface IIdentityService
 {
     Task<(bool Succeeded, Guid? UserId, IEnumerable<string> Errors)> RegisterUserAsync(string email, string password, string role, CancellationToken ct = default);
+    Task<(bool Succeeded, Guid? UserId, string? Email, IEnumerable<string> Roles)> ValidateCredentialsAsync(string email, string password, CancellationToken ct =default);
 }
